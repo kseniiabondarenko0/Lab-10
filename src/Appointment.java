f944694dba0cdff01f53fc49f664ec07d791efe0
Your Name <bondarenkoksenia2007@gmail.com>
2026-01-06 20:52:43 +0100

    Expand class Appointment and Nurse,

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,4 +1,33 @@
+import java.time.LocalDateTime;
+
 public class Appointment {
+    private static int appointmentCounter = 5000;
+    private int appointmentId;
+    private Patient patient;
+    private Doctor doctor;
+    private LocalDateTime dateTime;
+    private String reason;
+
+    public Appointment(Patient patient, Doctor doctor, LocalDateTime dateTime, String reason) {
+        this.appointmentId = appointmentCounter++;
+        this.patient = patient;
+        this.doctor = doctor;
+        this.dateTime = dateTime;
+        this.reason = reason;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
     public boolean overlaps(Appointment appointment) {
         return false;
     }
--- src/Nurse.java
+++ src/Nurse.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+
 public class Nurse extends Staff{
 
     public Nurse() {
@@ -11,6 +13,10 @@ public class Nurse extends Staff{
 
     public void checkVitals(Patient patient){
         System.out.println(getName() + " is checking vitals for patient " + patient.getName());
+    }
+
+    public Appointment createAppointment(Patient patient, Doctor doctor, LocalDateTime dateTime, String reason) {
+        return new Appointment(patient, doctor, dateTime, reason);
     }
 
 }
